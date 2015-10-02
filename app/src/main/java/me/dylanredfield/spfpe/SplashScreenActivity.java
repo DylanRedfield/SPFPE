cfbaef2e17bdf44395d98f960bb6ac37123f755c
Dylan Redfield <dylanredfield@verizon.net>
2015-10-02 12:27:52 -0400

    splash logic and sign in basic ui

---
+++ app/src/main/java/me/dylanredfield/spfpe/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package me.dylanredfield.spfpe;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.ParseUser;
+
+public class SplashScreenActivity extends Activity {
+
+    private ParseUser mCurrentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        setDefaultValues();
+        splashLogic();
+    }
+
+    private void setDefaultValues() {
+        mCurrentUser = ParseUser.getCurrentUser();
+    }
+
+    private void splashLogic () {
+
+        // Checks to see if user is logged in
+        if (mCurrentUser == null) {
+            // TODO intent to SignInActivity
+
+            Log.d("Cached User: ", "false");
+        } else {
+            // TODO intent to MainActivity
+            Log.d("Cached User: ", "true");
+        }
+    }
+}
