ce00e904033b5ba750ab6d535a0793af98a1891b
Dylan Redfield <dylanredfield@verizon.net>
2015-10-02 12:00:43 -0400

    Keys file finished

--- app/src/main/java/me/dylanredfield/spfpe/Keys.java
+++ app/src/main/java/me/dylanredfield/spfpe/Keys.java
@@ -9,9 +9,86 @@ public class Keys {
     public static String AUTH_DATA = "authData";
 
     // User
+    public static String USER_KEY = "_User";
     public static String USERNAME_STR = "username";
     public static String PASSWORD_STR = "password";
     public static String EMAIL_VERIFIED_BOOL = "emailVerified";
     public static String USER_TYPE_POINT = "UserType";
-    // public static String email
+    public static String EMAIL_STR = "email";
+    public static String FIRST_NAME_STR = "firstName";
+    public static String LAST_NAME_STR = "lastName";
+
+    // Admin
+    public static String ADMIN_KEY = "Admin";
+    public static String USER_POINT = "User";
+
+    // Assignment
+    public static String ASSIGNMENT_KEY = "Assignment";
+    public static String CLASS_POINT = "Class";
+    public static String LINK_STR = "link";
+    public static String ASSIGNMENT_NAME_STR = "assignmentName";
+
+    // Class
+    public static String CLASS_KEY = "Class";
+    public static String PERIOD_POINT = "Period";
+    public static String SCHOOL_YEAR_POINT = "SchoolYear";
+    public static String TEACHER_POINT = "Teacher";
+    public static String MARKING_PERIOD_NUM = "markingPeriod";
+    public static String SPORT_POINT = "Sport";
+
+    // CurrentSchoolYear
+    public static String CURRENT_SCHOOL_YEAR_KEY = "SchoolYear";
+    /* public static String SCHOOL_YEAR_POINT = "SchoolYear"; */
+
+    // Event
+    public static String EVENT_KEY = "Event";
+    public static String FIELD_NAMES_ARR = "fieldNames";
+    public static String NAME_STR = "name";
+    public static String NUM_FIELDS_NUM = "numFields";
+
+    // FitnessTest
+    public static String FITNESS_TEST_KEY = "FitnessTest";
+    /* public static String CLASS_POINT = "Class"; */
+    public static String EVENT_POINT = "Event";
+    public static String STUDENT_POINT = "Student";
+    public static String RESULTS_ARR = "results";
+    public static String TEST_NUMBER_NUM = "testNumber";
+
+    // GymMakeup
+    public static String GYM_MAKEUP_KEY = "GymMakeup";
+    /* public static String CLASS_POINT = "Class"; */
+    /* public static String STUDENT_POINT = "Student"; */
+    public static String DATE_DATE = "date";
+    public static String MINUTES_LOGGED_NUM = "minutesLogged";
+
+    // Notification
+    public static String NOTIFICATION_KEY = "Notification";
+    public static String CLASSES_REL = "Classes";
+
+    // Period
+    public static String PERIOD_KEY = "Period";
+    public static String PERIOD_NAME_STR = "periodName";
+
+    // SchoolYear
+    public static String SCHOOL_YEAR_KEY = "SchoolYear";
+    public static String YEAR_STR = "year";
+
+    // Sport
+    public static String SPORT_KEY = "Sport";
+    /* public static String NAME_STR = "name"; */
+
+    // Student
+    public static String STUDENT_KEY = "Student";
+    /* public static String CLASSES_REL = "Classes"; */
+    /* public static String USER_POINT = "User"; */
+    public static String GRADE_NUM = "grade";
+    public static String SELECTED_CLASS_POINT = "selectedClass";
+
+    // Teacher
+    public static String TEACHER_KEY = "Teacher";
+    /* public static String USER_POINT = "User"; */
+
+    // UserType
+    public static String USER_TYPE_KEY = "UserType";
+    public static String TYPE_STR = "type";
 }
