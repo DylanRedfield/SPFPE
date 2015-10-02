bd0c6270d705ca51e059812b760a1486aaa420d6
Dylan Redfield <dylanredfield@verizon.net>
2015-10-02 00:11:03 -0400

    test

---
+++ app/src/main/java/me/dylanredfield/spfpe/Keys.java
@@ -0,0 +1,17 @@
+package me.dylanredfield.spfpe;
+
+public class Keys {
+
+    // General
+    public static String APP_ID = "Bk54d6P86dY3nEy8NLmzzJ79IFEzNgqfWpDBspLk";
+    public static String CLIENT_KEY = "YbsUAhmuEUFteioZ7ggyvAdBG0IAsLaU5ynWcbKV";
+    public static String OBJECT_ID_STR = "objectId";
+    public static String AUTH_DATA = "authData";
+
+    // User
+    public static String USERNAME_STR = "username";
+    public static String PASSWORD_STR = "password";
+    public static String EMAIL_VERIFIED_BOOL = "emailVerified";
+    public static String USER_TYPE_POINT = "UserType";
+    // public static String email
+}
---
+++ app/src/main/java/me/dylanredfield/spfpe/SPFPE.java
@@ -0,0 +1,16 @@
+package me.dylanredfield.spfpe;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class SPFPE extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(getApplicationContext(), Keys.APP_ID, Keys.CLIENT_KEY);
+    }
+}
