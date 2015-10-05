a4ab7cc269e9123968a047c4d0aad98c22f57b94
Dylan Redfield <dylanredfield@verizon.net>
2015-10-05 12:41:19 -0400

    SignUpActivity Work

---
+++ app/src/main/java/me/dylanredfield/spfpe/SignUpFragment.java
@@ -0,0 +1,93 @@
+package me.dylanredfield.spfpe;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SignUpFragment extends Fragment {
+    private View mView;
+    private EditText mFirstName;
+    private EditText mLastName;
+    private EditText mPassword;
+    private EditText mConfirmPassword;
+    private Button mEnter;
+    private TextView mAlreadyHave;
+    private ParseUser mCurrentUser;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedState) {
+        mView = inflater.inflate(R.layout.fragment_sign_up, null, false);
+
+        setDefaultValues();
+        return mView;
+    }
+
+    private void setDefaultValues() {
+        mFirstName = (EditText) mView.findViewById(R.id.first_name);
+        mLastName = (EditText) mView.findViewById(R.id.last_name);
+        mPassword = (EditText) mView.findViewById(R.id.password);
+        mConfirmPassword = (EditText) mView.findViewById(R.id.confirm_password);
+
+        mEnter = (Button) mView.findViewById(R.id.enter);
+
+        mAlreadyHave = (TextView) mView.findViewById(R.id.already_have_account);
+    }
+
+    private void setListeners() {
+        mEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkInputs()) {
+                    signUp();
+                }
+            }
+        });
+    }
+
+    private boolean checkInputs() {
+        if (isOneWord(mFirstName.getText().toString().trim())) {
+            if (isOneWord(mLastName.getText().toString().trim())) {
+                if (mPassword.getText().toString().trim()
+                        .equals(mConfirmPassword.getText().toString().trim())) {
+                    return true;
+                } else {
+                    Helpers.createDialog(getActivity(), "Whoops", "Passwords Do Not Match");
+                }
+            } else {
+                Helpers.createDialog(getActivity(), "Whoops", "Last name can only be one word");
+            }
+        } else {
+            Helpers.createDialog(getActivity(), "Whoops", "First name can only be one word");
+        }
+        return false;
+    }
+
+    private boolean isOneWord(String s) {
+        Pattern pattern = Pattern.compile("\\s");
+        Matcher matcher = pattern.matcher(s);
+        return matcher.find();
+    }
+
+    private void signUp() {
+        mCurrentUser = new ParseUser();
+        //mCurrentUser.setUsername();
+        mCurrentUser.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+
+            }
+        });
+    }
+}
